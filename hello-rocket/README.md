ba487354cb2c783e33358ce175e68551e60104a1
Phodal Huang <h@phodal.com>
2020-07-05 21:22:39 +0800

    feat: make docker works

--- hello-rocket/README.md
+++ hello-rocket/README.md
@@ -3,5 +3,9 @@
 build:
 
 ```bash
-docker build -t hello-rocket .
+docker build -t hello-world .
+```
+
+```bash
+docker run --name hello-world -p 8080:8080 -d hello-world
 ```
--- hello-rocket/src/main.rs
+++ hello-rocket/src/main.rs
@@ -1,13 +1,46 @@
-#![feature(proc_macro_hygiene, decl_macro)]
+#![deny(warnings)]
+extern crate hyper;
+extern crate pretty_env_logger;
 
-#[macro_use] extern crate rocket;
-
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
-}
+use hyper::{Body, Response, Server};
+use hyper::service::service_fn_ok;
+use hyper::rt::{self, Future};
+use std::env;
 
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    pretty_env_logger::init();
+
+    let mut port: u16 = 8080;
+    match env::var("PORT") {
+        Ok(p) => {
+            match p.parse::<u16>() {
+                Ok(n) => {port = n;},
+                Err(_e) => {},
+            };
+        }
+        Err(_e) => {},
+    };
+    let addr = ([0, 0, 0, 0], port).into();
+
+    let new_service = || {
+        service_fn_ok(|_| {
+
+            let mut hello = "Hello ".to_string();
+            match env::var("TARGET") {
+                Ok(target) => {hello.push_str(&target);},
+                Err(_e) => {hello.push_str("World")},
+            };
+
+            Response::new(Body::from(hello))
+        })
+    };
+
+    let server = Server::bind(&addr)
+        .serve(new_service)
+        .map_err(|e| eprintln!("server error: {}", e));
+
+    println!("Listening on http://{}", addr);
+
+    rt::run(server);
 }
 
