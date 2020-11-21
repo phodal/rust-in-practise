68367a98c36b5009483084aad9d2838f20f81042
Phodal Huang <h@phodal.com>
2020-11-21 23:30:43 +0800

    feat: try to change to vectors

--- mut-vector/src/main.rs
+++ mut-vector/src/main.rs
@@ -6,13 +6,13 @@ const RULE_SIZE: usize = 100;
 
 trait BasicRule: DynClone {
     fn id(&self) -> i32;
-    fn collect_patterns_recursive(&mut self, container: &mut RuleContainer,);
+    fn collect_patterns_recursive(&mut self, container: &mut RuleContainer);
 }
 
 clone_trait_object!(BasicRule);
 
 #[derive(Clone)]
-pub struct Rule { id: i32, }
+pub struct Rule { id: i32 }
 
 #[derive(Clone)]
 pub struct BeginRule { rule: Rule }
@@ -34,44 +34,42 @@ impl BasicRule for BeginRule {
 }
 
 #[derive(Clone)]
-pub struct EmptyRule { }
+pub struct EmptyRule {}
 
 impl BasicRule for EmptyRule {
     fn id(&self) -> i32 { 0 }
-    fn collect_patterns_recursive(&mut self, container: &mut RuleContainer) {
-
-    }
+    fn collect_patterns_recursive(&mut self, container: &mut RuleContainer) {}
 }
 
-pub struct RuleContainer {
-    pub rules: HashMap<i32, Box<dyn BasicRule>>,
+pub struct RuleContainer<'rules> {
+    pub rules: &'rules mut Vec<Box<dyn BasicRule>>,
 }
 
-impl RuleContainer {
-    fn default() -> Self {
+impl<'rules> RuleContainer<'rules> {
+    fn default(rules: &'rules mut Vec<Box<dyn BasicRule>>) -> Self {
         RuleContainer {
-            rules: Default::default(),
+            rules
         }
     }
 
-    pub fn get_rule(&mut self, pattern_id: i32) -> &mut Box<dyn BasicRule> {
-        return self
-            .rules
-            .get_mut(&pattern_id)
-            .unwrap();
+    pub fn get_rule(&mut self, pattern_id: usize) -> &mut Box<dyn BasicRule> {
+        return &mut self
+            .rules[pattern_id];
     }
 
     pub fn register_rule(&mut self, result: Box<dyn BasicRule>) -> i32 {
         let id = result.id();
-        self.rules.insert(id, result);
+        self.rules.resize_with((id + 1) as usize, || { Box::from(EmptyRule {}) });
+        self.rules[id as usize] = result;
         id
     }
 }
 
 fn main() {
-    let mut container = RuleContainer::default();
-    let id = 1;
-    container.register_rule(Box::new(EmptyRule { }));
+    let mut rules = vec![];
+    let mut container = RuleContainer::default(&mut rules);
+
+    container.register_rule(Box::new(EmptyRule {}));
     container.register_rule(Box::new(BeginRule::new(1)));
 
     let mut rule = container.get_rule(1).clone();
