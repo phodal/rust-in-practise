f9daa844b28cbcb757f5a852bc94d5ad286bbc85
Phodal Huang <h@phodal.com>
2020-11-21 17:50:16 +0800

    feat: make it close to reality

--- mut-vector/src/main.rs
+++ mut-vector/src/main.rs
@@ -1,14 +1,19 @@
 use std::borrow::BorrowMut;
+use dyn_clone::{clone_trait_object, DynClone};
 
 const RULE_SIZE: usize = 100;
 
-trait BasicRule {
+trait BasicRule: DynClone {
     fn id(&self) -> i32;
     fn collect_patterns_recursive(&mut self, container: &mut RuleContainer,);
 }
 
+clone_trait_object!(BasicRule);
+
+#[derive(Clone)]
 pub struct Rule { id: i32, }
 
+#[derive(Clone)]
 pub struct BeginRule { rule: Rule }
 
 impl BeginRule {
@@ -21,10 +26,12 @@ impl BasicRule for BeginRule {
     fn id(&self) -> i32 { self.rule.id }
 
     fn collect_patterns_recursive(&mut self, container: &mut RuleContainer) {
-
+        let mut rule = container.get_rule(0).clone();
+        rule.collect_patterns_recursive(container);
     }
 }
 
+#[derive(Clone)]
 pub struct EmptyRule { }
 
 impl BasicRule for EmptyRule {
@@ -67,6 +74,6 @@ fn main() {
     let id = 1;
     container.register_rule(Box::new(BeginRule::new(id)));
 
-    let rule = container.get_rule(1);
+    let mut rule = container.get_rule(1).clone();
     rule.collect_patterns_recursive(&mut container);
 }
