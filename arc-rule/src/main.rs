5e2a2b66ae1defc0ed86bf1cacf487dbbb643e22
Phodal Huang <h@phodal.com>
2020-12-09 23:14:16 +0800

    feat: testing in rule

--- arc-rule/src/main.rs
+++ arc-rule/src/main.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 use std::rc::Rc;
 use std::thread::LocalKey;
 
-use crate::rule::{BasicRule, Rule, BeginRule};
+use crate::rule::{BasicRule, BeginRule};
 
 mod rule;
 
@@ -25,7 +25,12 @@ impl Default for RuleContainer {
 }
 
 impl RuleContainer {
-    // pub fn get_rule(&mut self, pattern_id: i32) -> Rc<RefCell<dyn AbstractRule>> {}
+    pub fn get_rules<'a>() -> HashMap<i32, Rc<dyn BasicRule>> {
+        RULES.with(|w| {
+            w.borrow_mut().clone()
+        })
+    }
+
     pub fn register_rule(rule: Rc<dyn BasicRule>) {
         RULES.with(|w| {
             w.borrow_mut().insert(rule.id(), rule);
@@ -34,20 +39,13 @@ impl RuleContainer {
 }
 
 fn main() {
-    let mut container = RuleContainer::default();
-
     let begin_rule = BeginRule::new(1);
     let empty_rule = BeginRule::new(2);
 
     RuleContainer::register_rule(Rc::new(begin_rule));
     RuleContainer::register_rule(Rc::new(empty_rule));
 
-
-    container.refs.with(|rules| {
-        // let first = rules.borrow().get(&1).unwrap();
-        // let second = rules.borrow().get(&2).unwrap();
-
-        // println!("{:?}", first.id());
-        // println!("{:?}", second.id());
-    })
+    let mut map = RuleContainer::get_rules();
+    let rule = map.get_mut(&1).unwrap();
+    println!("{:?}", rule.id());
 }
