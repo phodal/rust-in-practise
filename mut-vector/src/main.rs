1a00b1b501a6672c9ad148cce97d9d6aa866b938
Phodal Huang <h@phodal.com>
2020-12-09 19:52:16 +0800

    feat: try the new methods for ref

--- mut-vector/src/main.rs
+++ mut-vector/src/main.rs
@@ -4,18 +4,48 @@ use std::{cell::RefCell, collections::HashMap};
 use crate::rule::{EmptyRule, BeginRule, BasicRule};
 use std::rc::Rc;
 
+thread_local! {
+    pub static RULES: RefCell<HashMap<i32, Rc<dyn BasicRule>>> = RefCell::new(HashMap::new());
+}
+
+
 fn main() {
-    // https://github.com/servo/servo/blob/master/ports/winit/app.rs
     let first_rule = EmptyRule {};
     let second_rule = BeginRule::new(1);
 
-    let mut map: HashMap<i32, Rc<RefCell<dyn BasicRule>>> = HashMap::new();
+    register_rule(Rc::new(first_rule));
+    register_rule(Rc::new(second_rule));
 
-    map.insert(1, Rc::new(RefCell::new(second_rule)));
-    map.insert(2, Rc::new(RefCell::new(first_rule)));
-    {
-        let a = map.get(&1).unwrap().clone();
-        let x = &mut *a.borrow_mut();
-        x.collect_patterns_recursive(map);
-    }
+    RULES.with( | rules| {
+        let rule = rules.borrow_mut().get_mut(&1).unwrap();
+
+        match rules.borrow_mut().get(&1) {
+            None => {}
+            Some(rule) => {
+                println!("{:?}", rule.id());
+            }
+        }
+    });
 }
+
+pub fn register_rule(rule: Rc<dyn BasicRule>) {
+    RULES.with(|w| {
+        w.borrow_mut().insert(rule.id(), rule);
+    });
+}
+
+// fn refcell() {
+// // https://github.com/servo/servo/blob/master/ports/winit/app.rs
+//     let first_rule = EmptyRule {};
+//     let second_rule = BeginRule::new(1);
+//
+//     let mut map: HashMap<i32, Rc<RefCell<dyn BasicRule>>> = HashMap::new();
+//
+//     map.insert(1, Rc::new(RefCell::new(second_rule)));
+//     map.insert(2, Rc::new(RefCell::new(first_rule)));
+//     {
+//         let a = map.get(&1).unwrap().clone();
+//         let x = &mut *a.borrow_mut();
+//         x.collect_patterns_recursive(map);
+//     }
+// }
--- mut-vector/src/rule.rs
+++ mut-vector/src/rule.rs
@@ -5,7 +5,7 @@ use std::cell::RefCell;
 
 pub trait BasicRule: DynClone {
     fn id(&self) -> i32;
-    fn collect_patterns_recursive(&mut self, container: HashMap<i32, Rc<RefCell<dyn BasicRule>>>);
+    fn collect_patterns_recursive(&mut self, container: &RefCell<HashMap<i32, Rc<dyn BasicRule>>>);
     fn update_by(&mut self) where Self: Sized;
 }
 
@@ -26,10 +26,10 @@ impl BeginRule {
 impl BasicRule for BeginRule {
     fn id(&self) -> i32 { self.rule.id }
 
-    fn collect_patterns_recursive(&mut self, map: HashMap<i32, Rc<RefCell<dyn BasicRule>>>) {
-        let rule_ref = map.get(&1).unwrap().clone();
-        let x = &mut *rule_ref.borrow();
-        x.collect_patterns_recursive(map);
+    fn collect_patterns_recursive(&mut self, _map: &RefCell<HashMap<i32, Rc<dyn BasicRule>>>) {
+        // let rule_ref = map.get(&1).unwrap().clone();
+        // let x = &mut *rule_ref.borrow();
+        // x.collect_patterns_recursive(map);
     }
 
     fn update_by(&mut self) where Self: Sized {}
@@ -40,6 +40,6 @@ pub struct EmptyRule {}
 
 impl BasicRule for EmptyRule {
     fn id(&self) -> i32 { 0 }
-    fn collect_patterns_recursive(&mut self, _container: HashMap<i32, Rc<RefCell<dyn BasicRule>>>) {}
+    fn collect_patterns_recursive(&mut self, _container: &RefCell<HashMap<i32, Rc<dyn BasicRule>>>) {}
     fn update_by(&mut self) where Self: Sized {}
 }
