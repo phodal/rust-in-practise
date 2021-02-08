5f60c1e027ec51d28fcee50d20b74528b3c46444
Phodal Huang <h@phodal.com>
2021-02-08 10:25:27 +0800

    feat: make in different os for testings

--- plugins/my-master/src/main.rs
+++ plugins/my-master/src/main.rs
@@ -1,10 +1,36 @@
 use my_interface::SayHelloService;
+use libloading::Library;
+
+#[cfg(target_os = "linux")]
+fn load_library() -> Library {
+    unsafe {
+        return libloading::Library::new("target/debug/libmy_plugin.dylib")
+            .expect("load library");
+    }
+}
+
+#[cfg(target_os = "macos")]
+fn load_library() -> Library {
+    unsafe {
+        return libloading::Library::new("target/debug/libmy_plugin.dylib")
+            .expect("load library");
+    }
+}
+
+#[cfg(target_os = "windows")]
+fn load_library() -> Library {
+    unsafe {
+        return libloading::Library::new("target/debug/libmy_plugin.dll")
+            .expect("load library");
+    }
+}
 
 fn main() {
-    let lib = libloading::Library::new("target/debug/my_plugin.dll")
-        .expect("load library");
+    let lib = load_library();
+
     let new_service: libloading::Symbol<extern "Rust" fn() -> Box<dyn SayHelloService>> = unsafe { lib.get(b"new_service") }
         .expect("load symbol");
+
     let service = new_service();
     service.say_hello();
     let service = new_service();
