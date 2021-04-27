bbc0bf5c27f3b370a511a1d77e2f5afc76123489
Phodal Huang <h@phodal.com>
2021-04-27 20:18:27 +0800

    build: add sleep for event

--- chan/src/main.rs
+++ chan/src/main.rs
@@ -1,12 +1,14 @@
 use crossbeam_channel::{unbounded, RecvError};
 use std::thread;
+use std::time::Duration;
 
 fn main() {
     let (rpc_sender, rpc_receiver) = unbounded();
 
     thread::spawn(move || {
        loop {
-           rpc_sender.send("hello");
+           let _result = rpc_sender.send("hello");
+           thread::sleep(Duration::from_secs(1));
        }
     });
 
