64651a58294c3f51606b74435d2bb404d9733ed1
Phodal Huang <h@phodal.com>
2021-04-27 20:16:59 +0800

    build: add samples

--- chan/src/main.rs
+++ chan/src/main.rs
@@ -1,3 +1,27 @@
+use crossbeam_channel::{unbounded, RecvError};
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    let (rpc_sender, rpc_receiver) = unbounded();
+
+    thread::spawn(move || {
+       loop {
+           rpc_sender.send("hello");
+       }
+    });
+
+    thread::spawn(move || {
+       loop {
+           match rpc_receiver.recv() {
+               Ok(str) => {
+                   println!("{:?}", str);
+               }
+               Err(_) => {}
+           }
+       }
+    });
+
+    loop {
+
+    }
 }
